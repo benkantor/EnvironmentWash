77e59ee1ff6204947d153783141bbe6ef70f7e7f
Brian Besmanoff <bbesmanoff@gmail.com>
2014-11-18 15:11:11 -0500

    Add ability to scroll to new comment box

--- frontend/app/scripts/controllers/project.js
+++ frontend/app/scripts/controllers/project.js
@@ -1,8 +1,17 @@
 'use strict';
 
 angular.module('environmentWashApp')
-  .controller('ProjectCtrl', ['$scope', '$routeParams','Project',
-    function ($scope,$routeParams,Project) {
+  .controller('ProjectCtrl', ['$scope', '$routeParams','Project', '$location', '$anchorScroll',
+    function ($scope,$routeParams,Project, $location, $anchorScroll) {
       $scope.project = Project.get({projectId: $routeParams.projectId}, function() {});
+
+      $scope.gotoCommentbox = function() {
+        if ($location.hash() === 'add-a-comment') {
+          $anchorScroll();
+        } else {
+          $location.hash('add-a-comment');
+        }
+
+      };
     }
   ]);
