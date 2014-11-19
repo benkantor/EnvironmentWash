5aeb7a59d244b1f940ac0e1a8cdbb75c38737e2e
Ben Kantor <bkantor01@gmail.com>
2014-11-19 10:52:47 -0500

    Modified project schema to include desciption and start and end times

--- api/models/project.js
+++ api/models/project.js
@@ -4,9 +4,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
-  title: String,
+  name: String,
   date: Date,
+  startTime: String,
+  endTime: String,
   location: String,
+  description: String,
   createdBy: String,
   volunteers: [{name: String}],
   comments: [{created: Date, comment: String, createdBy: String}]
